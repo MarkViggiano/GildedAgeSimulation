f60345d11211eb4455a1109ab14cdd08eb3eca42
MarkViggiano <markcviggiano@gmail.com>
2021-06-11 20:06:50 -0400

    Move Citizens Along Path

--- js/citizen.js
+++ js/citizen.js
@@ -14,6 +14,8 @@ class Citizen extends GameObject {
     this.destinationX = this.factoryX;
     this.destinationY = this.factoryY;
     this.status = "At Home";
+    this.movingOnPath = false;
+    this.path;
   }
 
   //tax should be a floating point value between 0 and 1
@@ -29,8 +31,22 @@ class Citizen extends GameObject {
     return this.x == this.destinationX && this.y == this.destinationY;
   }
 
+  //path is 2d array containing x,y of where next to move similar to how 'homes' is a 2d array of home x,y
+  moveOnPath(path) {
+    this.movingOnPath = true;
+    this.path = path;
+  }
+
+  onPath() {
+    return this.movingOnPath;
+  }
+
   move() {
-    if (this.atDestination()) return;
+    if (this.atDestination() && !this.onPath()) return; //done for optimizations when not moving on a path
+    if (this.onPath()) {
+      this.destinationX = this.path[0][0];
+      this.destinationY = this.path[0][1];
+    }
 
     this.xVel = 0;
     this.yVel = 0;
@@ -63,10 +79,9 @@ class Citizen extends GameObject {
       this.y = this.destinationY;
     }
 
-    //Just a test
-    if (!this.isMoving()) {
-      this.destinationX = this.homeX;
-      this.destinationY = this.homeY;
+    if (this.atDestination() && this.onPath()) {
+      this.path.shift();
+      if (this.path.length == 0) this.movingOnPath = false;
     }
   }
 
